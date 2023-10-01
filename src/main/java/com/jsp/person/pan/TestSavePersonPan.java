7066a4abd39267886dbe379d79074e007edbb377
Bishal Saini <sabaattungekar25@gmail.com>
2023-10-01 18:29:55 +0530

    one to one bi direction

---
+++ src/main/java/com/jsp/person/pan/TestSavePersonPan.java
@@ -0,0 +1,36 @@
+
+package com.jsp.person.pan;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestSavePersonPan {
+public static void main(String[] args) {
+	EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("vishu");
+	EntityManager entityManager=entityManagerFactory.createEntityManager();
+    EntityTransaction entityTransaction=entityManager.getTransaction(); 
+    
+    Person person=new Person(); 
+	person.setName("Sabaat"); 
+	person.setEmail("sabaat25@mail"); 
+	person.setCno(860660888); 
+	
+	
+	Pan pan=new Pan();
+	pan.setAddress("Nerul");
+	pan.setPerson(person);
+	
+	person.setPan(pan);
+
+	
+	
+	entityTransaction.begin(); 
+	entityManager.persist(person); 
+	entityManager.persist(pan);
+	entityTransaction.commit();//commit means Action needs to be /perform
+	
+}
+}
+
